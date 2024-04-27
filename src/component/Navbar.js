2fe5f49f4be2c17e821b6b364d7fd5d98ec0ff06
Ankurdyno <98178905+Ankurdyno@users.noreply.github.com>
2024-04-27 17:26:03 +0530

    v-1

---
+++ src/component/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div>Header</div>
+      <div className="links">
+        <Link className="link" to="/signup">Signup</Link>
+        <Link className="link" to="/profile">Profile</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
